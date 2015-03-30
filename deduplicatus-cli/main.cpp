b6c98ffb5c9d72533bfa1f05517c49433bab1366
Peter Lam <peter@retepmal.com>
2015-03-30 16:40:42 +0800

    Initial Commit

---
+++ deduplicatus-cli/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  deduplicatus-cli
+//
+//  Created by Cheuk Hin Lam on 30/3/15.
+//  Copyright (c) 2015 Peter Lam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
