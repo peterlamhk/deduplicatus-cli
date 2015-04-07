e168cee6e17274e13d65d220f48ecdf982466373
Mok Ka Chun <mkc188@users.noreply.github.com>
2015-04-07 18:48:58 +0800

    Revert to use localhost web frontend

--- deduplicatus-cli/Config.cpp
+++ deduplicatus-cli/Config.cpp
@@ -17,16 +17,16 @@ using namespace rapidjson;
 
 Config::Config() {
     user_json = false;
-
+    
     // set default values for system-specific variables
     client_cookie = "client.jar";
     client_data = "client.json";
 
     mode_file_manager = "file-manager";
     mode_deduplication = "deduplication";
-
-    web_front = "https://deduplicatus.retepmal.com";
-    // web_front = "http://localhost";
+    
+//    web_front = "https://deduplicatus.retepmal.com";
+    web_front = "http://localhost";
     path_status = "/client/status";
     path_signin = "/client/signin";
     path_signout = "/client/signout";
@@ -37,34 +37,34 @@ Config::Config() {
     path_upload = "/client/upload";
     path_commit = "/client/commit";
     path_refreshToken = "/client/refreshToken";
-
+    
     // load JSON file to read user's data (if any)
     const char * filename = client_data.c_str();
     FILE * jsonFile = fopen(filename, "r");
     long jsonSize;
     char * userdata;
-
+    
     if( jsonFile != NULL ) {
         // obtain file size
         fseek(jsonFile, 0, SEEK_END);
         jsonSize = std::ftell(jsonFile);
         rewind(jsonFile);
-
+        
         // allocate char array to contain the whole file
         userdata = (char *) malloc(sizeof(char) * jsonSize);
 
         // copy the file into array
         fread(userdata, 1, jsonSize, jsonFile);
-
+        
         // parse JSON
         Document d;
         d.Parse(userdata);
         free(userdata);
-
+        
         Value& v_uid = d["userid"];
         Value& v_lock = d["lock"];
         Value& v_mode = d["mode"];
-
+        
         // set as variables in this class
         user_id = v_uid.GetString();
         user_lock = v_lock.GetString();
@@ -72,7 +72,7 @@ Config::Config() {
         user_mode = v_mode.GetString();
         user_json = true;
     }
-
+    
     // close file handler
     fclose(jsonFile);
 };
